e23f75bd2feb6ab0770cfa6f09376d0fcdd9b725
John Yang-Sammataro <johnys@stanford.edu>
2012-12-23 16:58:44 -0500

    finish first two koans

--- about_asserts.rb
+++ about_asserts.rb
@@ -7,34 +7,42 @@ class AboutAsserts < EdgeCase::Koan
 
   # We shall contemplate truth by testing reality, via asserts.
   def test_assert_truth
-    assert false                # This should be true
+    assert true                # This should be true
   end
 
   # Enlightenment may be more easily achieved with appropriate
   # messages.
+  #
+  # Messages can be attached
   def test_assert_with_message
-    assert false, "This should be true -- Please fix this"
+    assert true, "A message"
   end
 
   # To understand reality, we must compare our expectations against
   # reality.
+  #
+  # == assert works
   def test_assert_equality
-    expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
     assert expected_value == actual_value
   end
 
   # Some ways of asserting equality are better than others.
+  #
+  # assert_equals is better
   def test_a_better_way_of_asserting_equality
-    expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
     assert_equal expected_value, actual_value
   end
 
   # Sometimes we will ask you to fill in the values
+  #
+  # You can do things literally
   def test_fill_in_values
-    assert_equal __, 1 + 1
+    assert_equal 2, 1 + 1
   end
 end
